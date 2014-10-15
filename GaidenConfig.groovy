111dff824c5d21a5b8660d82d15fdbd76599db57
vincent314 <marmin.vincent@gmail.com>
2014-10-15 23:49:53 +0200

    Init project with gaiden site files : cours1 + cours2

---
+++ GaidenConfig.groovy
@@ -0,0 +1,29 @@
+// The base title of page
+title = "Cours de japonais niveau 3a année 2014-2015"
+
+// The title of TOC
+tocTitle = "Table des matières"
+
+// The path of template file
+//templateFilePath = "templates/layout.html"
+
+// The path of TOC file
+//tocFilePath = "pages/toc.groovy"
+
+// The path of TOC output file
+//tocOutputFilePath = "toc.html"
+
+// The path of page source files directory
+//pagesDirectoryPath = "pages"
+
+// The path of static files directory
+//staticDirectoryPath = "static"
+
+// The path of directory to be outputted a document
+//outputDirectoryPath = "build/html"
+
+// The encoding of input Markdown resource
+//inputEncoding = "UTF-8"
+
+// The encoding of output document
+//outputEncoding = "UTF-8"
