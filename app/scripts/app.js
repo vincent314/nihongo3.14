d03a5d893492efa1758b948957c58e5307f3d0b4
vincent <vincent314@hotmail.fr>
2014-11-09 01:11:02 +0100

    Using showdown + lesson data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,20 @@
 'use strict';
 
 angular.module('nihongo', ['ngRoute'])
-    .config(['$routeProvider','CONFIG',function ($routeProvider, CONFIG) {
+  .config(['$routeProvider', 'CONFIG', function ($routeProvider, CONFIG) {
 
-        _(CONFIG.categories).forEach(function (category) {
-            _(category.pages).forEach(function (page) {
-                var route = "/" + getSlug(category.title) + "/" + getSlug(page.title);
-                $routeProvider.when(route, {
-                    templateUrl: category.dir + '/' + page.file,
-                    controller: 'PageController'
-                });
-            });
+    _(CONFIG.categories).forEach(function (category) {
+      _(category.pages).forEach(function (page) {
+        var route = '/' + getSlug(category.title) + '/' + getSlug(page.title);
+        $routeProvider.when(route, {
+          templateUrl: category.dir + '/' + page.file,
+          controller: 'PageController'
         });
-        $routeProvider
-            .otherwise({
-                templateUrl: CONFIG.toc.templateUrl,
-                controller: 'TocController'
-            });
-    }]);
+      });
+    });
+    $routeProvider
+      .otherwise({
+        templateUrl: CONFIG.toc.templateUrl,
+        controller: 'TocController'
+      });
+  }]);
