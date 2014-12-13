35a608d764a09e1c6ea71d843a297e0e3f65d78e
vincent <vincent314@hotmail.fr>
2014-12-13 17:54:17 +0100

    English & japanese translation and lang switch feature + publish

--- app/scripts/controllers/headerController.js
+++ app/scripts/controllers/headerController.js
@@ -1,4 +1,7 @@
 'use strict';
 
-angular.module('nihongo').controller('HeaderController',[function() {
+angular.module('nihongo').controller('HeaderController',['$scope','$translate',function($scope,$translate) {
+  $scope.switchLang= function(lang) {
+    $translate.use(lang);
+  };
 }]);
--- app/scripts/translate.js
+++ app/scripts/translate.js
@@ -12,7 +12,42 @@ angular.module('nihongo').config(['$translateProvider',function($translateProvid
     'LINKS':'Liens',
     'LESSONS_MANUAL':'Supports de cours',
     'YEAR_3':'3ième année',
-    'ALL':'Tous'
+    'ALL':'Tous',
+    'LANG':'Langue',
+    'TOC' : 'Table des matières',
+    'ABOUT' : 'À propos'
+  });
+  $translateProvider.translations('en',{
+    'HOME':'Home',
+    'LAST_LESSONS':'Latest lessons',
+    'DOWNLOADS':'Downloads',
+    'LESSONS_YEAR_1':'1st year lessons 2011/2012',
+    'LESSONS_YEAR_2':'2nd year lessons 2012/2013',
+    'LESSONS_YEAR_3A':'3rd year lessons 2013/2014',
+    'LESSONS_YEAR_3B':'3rd year lessons 2014/2015',
+    'LINKS':'Links',
+    'LESSONS_MANUAL':'Manuals',
+    'YEAR_3':'3rd year',
+    'ALL':'All',
+    'LANG':'Lang',
+    'TOC' : 'Table of content',
+    'ABOUT' : 'About'
+  });
+  $translateProvider.translations('jp',{
+    'HOME':'ホーム',
+    'LAST_LESSONS':'最近授業',
+    'DOWNLOADS':'ダウンロード',
+    'LESSONS_YEAR_1':'一年授業 2011/2012',
+    'LESSONS_YEAR_2':'二年授業 2012/2013',
+    'LESSONS_YEAR_3A':'三年授業 2013/2014',
+    'LESSONS_YEAR_3B':'三年授業 2014/2015',
+    'LINKS':'リンク',
+    'LESSONS_MANUAL':'教則本',
+    'YEAR_3':'三年',
+    'ALL':'',
+    'LANG':'言語',
+    'TOC' : '目次',
+    'ABOUT' : 'About'
   });
   $translateProvider.preferredLanguage('fr');
 }]);
