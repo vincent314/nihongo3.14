541cae7974ecd122ddd9ad9fe1e13f84838cb355
vincent <vincent314@hotmail.fr>
2015-10-31 10:57:48 +0100

    feat(build): add usemin and serve tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,16 @@ autoprefxer = require('gulp-autoprefixer'),
 livereload = require('gulp-livereload'),
 wiredep = require('gulp-wiredep'),
 Server = require('karma').Server,
-jasmineNode = require('gulp-jasmine-node');
+coffee = require('gulp-coffee'),
+jasmineNode = require('gulp-jasmine-node'),
+usemin = require('gulp-usemin'),
+uglify = require('gulp-uglify'),
+minifyHtml = require('gulp-minify-html'),
+rev = require('gulp-rev'),
+connect = require('gulp-connect');
+
+require('coffee-script/register');
+
 // Configurable paths
 var config = {
   tmp: '.tmp',
@@ -30,7 +39,8 @@ gulp.task('scripts', function () {
     config.app + '/scripts/vendor/*',
     'test/spec/**/*.js'])
     .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter(stylish));
+    .pipe(jshint.reporter(stylish))
+    .pipe(concat());
 });
 
 gulp.task('wiredep', function () {
@@ -42,24 +52,62 @@ gulp.task('wiredep', function () {
     .pipe(gulp.dest(config.app));
 });
 
-gulp.task('test', function (done) {
+gulp.task('karma', function (done) {
   new Server({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
-  }, done).start();
+  }, function (err) {
+    console.log(err);
+    done();
+  }).start();
 });
 
 gulp.task('jasmine-node', function () {
   return gulp.src(['test/node/**/*Spec.coffee'])
     .pipe(jasmineNode({
       timeout: 10000,
-      coffee: true,
       verbose: true
     }))
 });
 
+gulp.task('copy', function () {
+  return gulp.src([
+      '*.{ico,png,txt}',
+      'CNAME',
+      'images/{,*/}*.webp',
+      '{,*/}*.html',
+      'styles/fonts/{,*/}*.*',
+      'docs/**/*.*'
+    ], {cwd: config.app}
+  )
+    .pipe(gulp.dest(config.dist));
+});
+
 gulp.task('default', ['clean'], function () {
   return gulp.start('style', 'scripts');
+});
+
+gulp.task('build', function () {
+  return gulp.src(config.app + '/index.html')
+    .pipe(usemin({
+      cssvendor: [rev()],
+      cssmain: [rev()],
+      html: [minifyHtml({
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        removeAttributeQuotes: true,
+        removeCommentsFromCDATA: true,
+        removeEmptyAttributes: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true
+      })],
+      jsvendor: [uglify(), rev()],
+      jsplugins: [uglify(), rev()],
+      jsmain: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest(config.dist));
 });
 
 gulp.task('watch', function () {
@@ -71,3 +119,15 @@ gulp.task('watch', function () {
 
   gulp.watch(config.dist + '/**').on('change', livereload.changed);
 });
+
+gulp.task('webserver', function () {
+  connect.server({
+    livereload:true,
+    root:['.tmp','.',config.app],
+    port:9000,
+    host:'localhost',
+    open:true
+  });
+});
+
+gulp.task('serve',['webserver','watch']);
