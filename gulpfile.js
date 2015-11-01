dac4d3f626788b6616bb8f4bbec3508085cf80ae
vincent <vincent314@hotmail.fr>
2015-11-01 22:51:22 +0100

    feat(build): generate kanji lessons and epubs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 $ = require('gulp-load-plugins')(),
 del = require('del'),
 stylish = require('jshint-stylish'),
+fs = require('fs'),
 Server = require('karma').Server;
 
 require('coffee-script/register');
@@ -11,6 +12,10 @@ var config = {
   tmp: '.tmp',
   app: 'app',
   dist: 'output/nihongo3.14_gh-pages'
+};
+
+var onError = function (err) {
+  console.log(err);
 };
 
 gulp.task('clean', function (cb) {
@@ -72,22 +77,38 @@ gulp.task('copy', function () {
     .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('default', ['clean'], function () {
+gulp.task('default', ['clean','style','script','wiredep','karma','jasmine-node','docs:multiple'], function () {
   return gulp.start('style', 'scripts');
 });
 
-gulp.task('docs',function(){
-  gulp.src('docs/src/**/*.md')
+gulp.task('docs:multiple', function () {
+  return gulp.src('docs/src/**/*.md')
     .pipe($.showdown({
-      extensions:[require('showdown-furigana-extension'),'table']
+      extensions: [require('showdown-furigana-extension'), 'table']
     }))
     .pipe(gulp.dest(config.app + '/docs/html'))
-    .on('error',function(err) {
-      console.log(err);
+    .on('error', function (err) {
+      $.notify({message: err})
     });
 });
 
-gulp.task('build', ['clean','style','scripts'], function () {
+gulp.task('docs:single', function () {
+  return gulp.src(
+    require('./tasks/lib/configReader').getFileList('app/scripts/config.js', 'docs/html/Cours_3b'),
+    {cwd: 'docs/src/Cours_3b'}
+  )
+    .pipe($.debug())
+    .pipe($.plumber({
+      errorHandler: onError
+    }))
+    .pipe($.showdown({
+      extensions: [require('showdown-furigana-extension'), 'table']
+    }))
+    .pipe($.concat('single.html'))
+    .pipe(gulp.dest('.tmp/'));
+});
+
+gulp.task('build', ['clean', 'style', 'scripts', 'copy', 'docs:multiple', 'epub', 'epub-kanji'], function () {
   return gulp.src(config.app + '/index.html')
     .pipe($.usemin({
       cssvendor: [$.rev()],
@@ -112,8 +133,8 @@ gulp.task('build', ['clean','style','scripts'], function () {
 
 gulp.task('watch', function () {
   gulp.watch('bower.json', ['wiredep']);
-  //gulp.watch(config.app + '/scripts/**/*.js', ['scripts']);
-  //gulp.watch(config.app + '/styles/**/*.css', ['style']);
+  gulp.watch(config.app + '/scripts/**/*.js', ['scripts']);
+  gulp.watch('docs/src/**/*.md',['docs:multiple']);
 
   $.livereload.listen();
 
@@ -130,3 +151,76 @@ gulp.task('webserver', function () {
 });
 
 gulp.task('serve', ['webserver', 'watch']);
+
+gulp.task('kanji-lessons', function () {
+  return gulp.src(config.app + '/docs/kanji-lessons/partials/kanji-lessons.html')
+    .pipe($.debug())
+    .pipe($.mustache({
+        kanjis: JSON.parse(fs.readFileSync(config.app + '/docs/kanji-lessons/data/kanjis.json'))
+      }
+    ))
+    .pipe(gulp.dest(config.tmp + '/kanji-lessons'));
+});
+
+gulp.task('epub', ['docs:single'], function (done) {
+  // calibre must be installed and ebook-convert must exist.
+  var convert = require('ebook-convert');
+  var epub = convert({
+    source: '.tmp/single.html',
+    target: './app/docs/epub/Cours de japonais niveau 3 2014-2015.epub',
+    arguments: [
+      '--page-breaks-before', '//h:h1',
+      '--authors', 'Vincent M.',
+      '--title', 'Cours de japonais niveau 3 2014/2015',
+      '--level1-toc', '//h:h1'
+    ]
+  });
+
+  epub.on('end', function () {
+    console.log('Epub generated !');
+    done();
+  });
+  epub.on('error', function (res) {
+    console.log('Error : ' + res);
+    done();
+  });
+  epub.on('exit', function (res) {
+    if (res !== 0) {
+      done();
+      throw new Error('Epub creation Error (Error ' + res + ')');
+    }
+    done();
+  });
+});
+
+gulp.task('epub-kanji', ['kanji-lessons'], function (done) {
+  // calibre must be installed and ebook-convert must exist.
+  var convert = require('ebook-convert');
+  var epub = convert({
+    source: '.tmp/kanji-lessons/kanji-lessons.html',
+    target: './app/docs/kanji-lessons/epub/kanji-lessons.epub',
+    arguments: [
+      '--page-breaks-before', '//h:h2',
+      '--authors', 'Vincent M.',
+      '--title', 'Apprentissage des Kanji',
+      '--level1-toc', '//h:h2'
+    ]
+  });
+
+  epub.on('end', function () {
+    console.log('Epub generated !');
+    done();
+  });
+  epub.on('error', function (res) {
+    console.log('Error : ' + res);
+    done();
+  });
+  epub.on('exit', function (res) {
+    if (res !== 0) {
+      done();
+      console.log(res);
+      throw new Error('Epub creation Error (Error ' + res + ')');
+    }
+    done();
+  });
+});
