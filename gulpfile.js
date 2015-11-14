ca65eb7aed3511f7681467edfdaf1fa97339024c
vincent <vincent314@hotmail.fr>
2015-11-14 12:39:35 +0100

    feat(kanji): add plugin for kanji test form generation

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp'),
   del = require('del'),
   stylish = require('jshint-stylish'),
   fs = require('fs'),
-  Server = require('karma').Server;
+  Server = require('karma').Server,
+  through = require('through2'),
+  _ = require('lodash');
 
 require('coffee-script/register');
 
@@ -259,4 +261,41 @@ gulp.task('epub-kanji', ['kanji-lessons'], function (done) {
     }
     done();
   });
+});
+
+
+function kanjiTestPlugin(){
+  return through.obj(function(file,enc,callback){
+    if(file.isNull() || file.isDirectory()){
+      this.push(file);
+      return callback();
+    }
+
+    // No support for streams
+    if (file.isStream()) {
+      this.emit('error', new PluginError({
+        plugin: 'kanjiTestForm',
+        message: 'Streams are not supported.'
+      }));
+      return callback();
+    }
+
+    if(file.isBuffer()) {
+      var kanjis = JSON.parse(file.contents.toString());
+      var vocabulary = _.chain(kanjis)
+        .pluck('vocabulary')
+        .flatten()
+        .shuffle()
+        .value();
+      console.log(vocabulary);
+      this.push(file);
+      return callback();
+    }
+  });
+}
+
+gulp.task('kanjiTest',function(){
+  gulp.src(config.app + '/docs/kanji-lessons/data/kanjis.json')
+    .pipe(kanjiTestPlugin())
+    .pipe(gulp.dest('.tmp'));
 });
